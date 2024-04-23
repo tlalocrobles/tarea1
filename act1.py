e8e3f3c34f02e994ea88e9ac4dea67ee7e0391ea
TLALOX <161402783+tlalocrobles@users.noreply.github.com>
2024-04-22 19:59:58 -0600

    Update act1.py

--- act1.py
+++ act1.py
@@ -1,7 +1,36 @@
-import random
-caracteres = ("+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
-x=int(input("De cuantos caracteres quieres tu contraseña?"))
-z=""
-for i in range (x):
-    z+=random.choice(caracteres)
-print (z)
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+
+
+@bot.command()
+async def repetirMensaje(ctx,*,mesaje):
+    await ctx.send(mesaje)
+bot.run("MTIyOTU5ODc3OTcxMjM0NDExNA.G8q-T6.HHrO-azXNRw1Awes2VsYLA1xHjH7-2IVq0gmZ8")
